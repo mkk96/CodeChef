7e4f51cddd6efa20a09b50aa9885ecf97649960c
mkk96 <one.end4@gmail.com>
2021-07-26 10:31:05 +0530

    July Starters 7 2021

--- Ceiling Sum.cpp
+++ Ceiling Sum.cpp
@@ -1,29 +1,32 @@
+#include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
 	int t;
 	cin>>t;
 	while(t--)
 	{
-		double a,b;
-		long long int ans=0;
+		int a,b;
 		cin>>a>>b;
 		if(a==b)
 		{
-			ans=0;
+			cout<<"0"<<endl;
 		}
-		else if(a>b)
+		else if(b>a)
 		{
-			ans=(ceil((b-(a-1))/2)+ceil(((a-1)-a)/2));
+			cout<<(b-a)/2+1<<endl;
 		}
 		else
 		{
-			ans=(ceil((b-(a+1))/2)+ceil(((a+1)-a)/2));
+			if((a-b)%2==0)
+			{
+				cout<<(b-a)/2+1<<endl;
+			}
+			else
+			{
+				cout<<(b-a)/2<<endl;
+			}
 		}
-		cout<<ans<<endl;
 	}
-	return 0;
 }
